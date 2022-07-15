ed912ebddf03e96c01ac3415074d4e3d9b91e384
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2022-07-15 17:54:09 -0400

    small changes

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,4 +1,16 @@
 
+function sendMail() {
+    var params = {
+        from_name : document.getElementById("name").value,
+        email_id : document.getElementById("email_id").value,
+        message : document.getElementById("message").value
+    }
+
+    emailjs.send("service_vqnoby5","template_is1ti5m", params).then(function (res) {
+        console.log("Success " + res.status);
+    })
+}
+
 var submit_button = document.getElementById("btn-submit");
 
 submit_button.addEventListener("click", function(e) {
@@ -29,51 +41,5 @@ var monthNames = [ "January", "February", "March", "April", "May", "June",
 var dayNames= ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
 
 var newDate = new Date();
-newDate.setDate(newDate.getDate());
+newDate.setDate(newDate.getDate() );
 $('.date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()]);
-
-function validateForm() {
-    var name =  document.getElementById('name').value;
-    if (name == "") {
-        document.querySelector('.status').innerHTML = "Name cannot be empty";
-        return false;
-    }
-    var email =  document.getElementById('email').value;
-    if (email == "") {
-        document.querySelector('.status').innerHTML = "Email cannot be empty";
-        return false;
-    } else {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(!re.test(email)){
-            document.querySelector('.status').innerHTML = "Email format invalid";
-            return false;
-        }
-    }
-    var message =  document.getElementById('message').value;
-    if (message == "") {
-        document.querySelector('.status').innerHTML = "Message cannot be empty";
-        return false;
-    }
-    document.getElementById('status').innerHTML = "Sending...";
-    formData = {
-        'name': $('input[name=name]').val(),
-        'email': $('input[name=email]').val(),
-        'message': $('textarea[name=message]').val()
-    };
-
-
-    $.ajax({
-        url: "php/mail.php",
-        type: "POST",
-        data: formData,
-        success: function (data, textStatus, jqXHR) {
-
-            $('#status').text(data.message);
-            if (data.code) //If mail was sent successfully, reset the form.
-                $('#contact-form').closest('form').find("input[type=text], textarea").val("");
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            $('#status').text(jqXHR);
-        }
-    });
-}
